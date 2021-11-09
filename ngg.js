48f4b5bc9d9422eebac37cc224d772021e72874a
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-09 15:48:43 -0400

    Added preventDefault method to parameter and added reset button.

--- ngg.js
+++ ngg.js
@@ -1,23 +1,36 @@
 let x = Math.floor((Math.random() * 100) + 1);
 	console.log(x);
 
-function checkInput() {
+function checkInput(event) {
 	const inputValue = document.getElementById("num").value;
 	let numberOfSub = 0;
-	const parr = document.getElementById("demo");
+	const parr = document.getElementById("demo");	
 	if(inputValue == x && numberOfSub <= 3) {
 		parr.innerHTML = "Congratulations, you got it right!"
+		resetButton();
 	} else if(inputValue < x && numberOfSub < 3) {
 		parr.innerHTML = "Too Low"
 	} else if(inputValue > x && numberOfSub < 3) {
 		parr.innerHTML = "Too high"
 	} else {
 		parr.innerHTML = `Sorry, the correct number was ${x}. Try again.`
+		resetButton();	
 	}
-	numberOfSub++
 
+	numberOfSub++;
+	event.preventDefault();
+		
 };
 
 
+let resetGame = function () {
+document.location.href = "";
+};
 
+function resetButton() {
+	var y = document.createElement("BUTTON");
+  	y.addEventListener("click", resetGame);
+ 	y.innerHTML	= "Try Again";
+  	document.body.appendChild(y);
+  };
 
