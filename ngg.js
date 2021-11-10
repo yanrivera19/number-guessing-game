c333ff6f7628410ae0e0890d5f7758db93e8a142
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-10 00:04:51 -0400

    Number of submits variable was moved to the global scope. Successfully established limited amount of submits by user. Edited HTML page.

--- ngg.js
+++ ngg.js
@@ -1,26 +1,36 @@
 let x = Math.floor((Math.random() * 100) + 1);
 	console.log(x);
+let numberOfSub = 0;
+const maxSubmits = 3;
 
 function checkInput(event) {
 	const inputValue = document.getElementById("num").value;
-	let numberOfSub = 0;
 	const parr = document.getElementById("demo");	
-	if(inputValue == x && numberOfSub <= 3) {
+	if(inputValue == x && numberOfSub <= maxSubmits) {
 		parr.innerHTML = "Congratulations, you got it right!"
 		resetButton();
-	} else if(inputValue < x && numberOfSub < 3) {
+	} else if(inputValue < x && numberOfSub < maxSubmits) {
 		parr.innerHTML = "Too Low"
-	} else if(inputValue > x && numberOfSub < 3) {
+		numberOfSub++;
+	} else if(inputValue > x && numberOfSub < maxSubmits) {
 		parr.innerHTML = "Too high"
+		numberOfSub++;		
 	} else {
 		parr.innerHTML = `Sorry, the correct number was ${x}. Try again.`
 		resetButton();	
 	}
 
 	numberOfSub++;
-	event.preventDefault();
-		
+	console.log(numberOfSub);
+	event.preventDefault();		
 };
+
+const enterKey = document.getElementById("inputPare");
+enterKey.addEventListener('keypress', function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+  }
+});
 
 
 let resetGame = function () {
@@ -29,8 +39,10 @@ document.location.href = "";
 
 function resetButton() {
 	var y = document.createElement("BUTTON");
+	y.className = "btn btn-success";
+  	y.innerHTML	= "Play Again";
+  	document.querySelector(".container-fluid").appendChild(y);
   	y.addEventListener("click", resetGame);
- 	y.innerHTML	= "Try Again";
-  	document.body.appendChild(y);
+  	document.getElementById("buttonSub").disabled = true;
   };
 
