44525cec610927a0ef3d25334d1f27b353d35c35
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-12 17:29:18 -0400

    Changed the way the game restarts. Instead of refreshing page, it will reset the variables to their initial values and clear out the messages displayed.

--- ngg.js
+++ ngg.js
@@ -1,30 +1,34 @@
 /*
 Number-guseeing-Game
 
-Step 1: Define a variable called x with a random number producer, using
+Step 1: Define a variable called randomNum with a random number producer, using
 the Math.random method to generate the random number from 1-100, and apply
 to it the Math.floor method to make the random numbers integers and not decimals.
 Step 2: Define a variable called numberOfSub with an initial value of 1. We will use
 this variable to track the amount of submits that have been made in the browser.
-Step 3: Define a variable called maxSubmits with the maximum number of submits 
+Step 3: Define a variable called maxNumSubmits with the marandomNumimum number of submits 
 allowed, which is 3.
-Step 4: Create a function called checkInput that takes in an event as a parameter.
+Step 4: Create a function called checkInput that takes in an event as a parameter. On our HTML page, 
+we add an onsubmit attribute to the form element so that it executes this function whenever a submit
+is made. We also add an onfocus attribute to the input element where the users will write their numbers,
+so that whenever the input box gets cleared whenever clicked and without the use of the backspace or delete
+keys.
 Step 5: Define a variable called inputValue with the selection of the input element's
 value.
-Step 6: Define a variable called parr with the selection of a p element in out HTML file
+Step 6: Define a variable called messageDisp with the selection of a p element in out HTML file
 that will display a message.
 Step 7: The function will check several conditions. In general, the purpose of the game
 is to guess the random number that the computer selected in no more than 3 attempts. So
 firstly we will check if the inputValue received is equal to the random number and if the
-numberOfSub is less or equal to the maxSubmits(3). If this is true, we will display on our 
+numberOfSub is less or equal to the maxNumSubmits(3). If this is true, we will display on our 
 selected p element a message of congratulations and we will call a function called resetButton
 that will disable the submit button and display a button on the page that can start the game 
 again. This function will be defined later on.
 Step 8: We will check if the inputValue is less than the random number and if the numberOfSub
-is less that the maxSubmits. If this is true, we will display a message to the browser stating 
+is less that the maxNumSubmits. If this is true, we will display a message to the browser stating 
 that the input value submitted was too low.
 Step 9: We will check if the inputValue is greater than the random number and if the numberOfSub
-is greater that the maxSubmits. If this is true, we will display a message to the browser stating 
+is greater that the maxNumSubmits. If this is true, we will display a message to the browser stating 
 that the input value submitted was too high.
 Step 10: If none of these previous conditions are met, then we will display a message to the browser
 indicating that the game is over and giving the option to play again. The resetButton function will
@@ -33,51 +37,61 @@ Step 11: As long as the user has more chances to submit a guess, the numberOfSub
 Step 12: We will use the preventDefault method on the event parameter to prevent the form to be submitted.
 This will be used so that the random number doesn't change with every submission and the messages 
 displayed on the page can be visible after every submission. 
-Step 13: Create a function called resetGame that will refresh the page whenever executed.
-Step 14: Create the function called resetButton that will create and display a button element whenever 
-executed. We add to it an addEventListener method with "click" and the resetGame function as its 
-parameters. This will execute the resetGame function whenever the newly created button gets clicked.
-We also select the submit button by its Id name, add to it the disabled property, and assign to it the 
-boolean value true. What this will do is that whenever the resetButton function gets executed, the submit 
+Step 13: Create a button element and store it in a variable called resetBtn. This button will not be displayed
+until the resetButton gets executed.
+Step 14: Create a function called resetGame that will generate a new random number and will
+reset the numberOfSub variable to its initial value of 1, will clear out the displayed messages using an empty
+string, will enable the use of the submit button (which will be disabled in the next function), and will hide the
+button element that will be created in the resetButton function.
+Step 15: Create the function called resetButton that will display the resetBtn, and will make the resetBtn execute the 
+resetGame funciton whenever clicked. It will also select the submit button by its Id name, add to it the disabled property,
+and assign to it the boolean value true. What this will do is that whenever the resetButton function gets executed, the submit 
 button will get disabled and therefore the "Play Again" button created by the resetButton function will get 
-displayed only once, and not everytime you either press the enter-key or click the submit button.
+displayed once, and not everytime you either press the enter-key or click the submit button.
 */
 
-let x = Math.floor((Math.random() * 100) + 1);
-	console.log(x);
+let randomNum = Math.floor((Math.random() * 100) + 1);
+	console.log(randomNum);
 let numberOfSub = 1;
-const maxSubmits = 3;
+let maxNumSubmits = 3;
+let inputValue = document.getElementById("num").value;
+const messageDisp = document.getElementById("demo");
 
-function checkInput(event) {
-	const inputValue = document.getElementById("num").value;
-	const parr = document.getElementById("demo");	
-	if(inputValue == x && numberOfSub <= maxSubmits) {
-		parr.innerHTML = "Congratulations, you got it right!"
+function checkInput(event) {		
+	if(inputValue == randomNum && numberOfSub <= maxNumSubmits) {
+		messageDisp.innerHTML = "Congratulations, you got it right!"
 		resetButton();
-	} else if(inputValue < x && numberOfSub < maxSubmits) {
-		parr.innerHTML = "Too Low"
-	} else if(inputValue > x && numberOfSub < maxSubmits) {
-		parr.innerHTML = "Too high"		
+	} else if(inputValue < randomNum && numberOfSub < maxNumSubmits) {
+		messageDisp.innerHTML = "Too Low"
+	} else if(inputValue > randomNum && numberOfSub < maxNumSubmits) {
+		messageDisp.innerHTML = "Too high"		
 	} else {
-		parr.innerHTML = `Sorry, the correct number was ${x}. Try again.`
+		messageDisp.innerHTML = `Sorry, the correct number was ${randomNum}. Try again.`
 		resetButton();	
 	}
-	
+
 	numberOfSub++
 	event.preventDefault();		
 };
 
+let resetBtn = document.createElement("BUTTON");
+resetBtn.className = "btn btn-success";
+resetBtn.innerHTML	= "Play Again";
+document.querySelector(".container-fluid").appendChild(resetBtn);
+resetBtn.style.display = "none";
 
 let resetGame = function () {
-document.location.href = "";
+	randomNum = Math.floor((Math.random() * 100) + 1);
+	numberOfSub = 1;
+	inputValue = document.getElementById("num").value = "";
+	document.getElementById("buttonSub").disabled = false;
+	resetBtn.style.display= "none";
+	messageDisp.innerHTML = "";
 };
 
 function resetButton() {
-	const y = document.createElement("BUTTON");
-	y.className = "btn btn-success";
-  	y.innerHTML	= "Play Again";
-  	document.querySelector(".container-fluid").appendChild(y);
-  	y.addEventListener("click", resetGame);
+	resetBtn.style.display = "inline-block";	
+  	resetBtn.addEventListener("click", resetGame);
   	document.getElementById("buttonSub").disabled = true;
-  };
+};
 
